278a6af0d723fa7a9252f6d69538fca88078468f
Luobata <602975805@qq.com>
2014-08-08 11:56:49 +0800

    Revert "Revert "StudentInfoManage""
    This reverts commit 202f34a8fb52113da3720aee758c3ddaa3ec4be9.

---
+++ src/com/luobata/web/GradeListServlet.java
@@ -0,0 +1,64 @@
+package com.luobata.web;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+import com.luobata.dao.GradeDao;
+import com.luobata.model.Grade;
+import com.luobata.model.PageBean;
+import com.luobata.util.DbUtil;
+import com.luobata.util.JsonUtil;
+import com.luobata.util.ResponseUtil;
+
+public class GradeListServlet extends HttpServlet{
+	DbUtil dbutil=new DbUtil();
+	GradeDao gradeDao=new GradeDao();
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		this.doPost(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String page=request.getParameter("page");
+		String rows=request.getParameter("rows");
+		String gradeName=request.getParameter("gradeName");
+		if(gradeName==null){
+			gradeName="";
+		}
+		Grade grade=new Grade();
+		grade.setGradeName(gradeName);
+		PageBean pageBean= new PageBean(Integer.parseInt(page),Integer.parseInt(rows));
+		Connection con=null;
+		try{
+			con=dbutil.getCon();
+			JSONObject result=new JSONObject();
+			JSONArray jsonArray=JsonUtil.formatRsToJsonArray(gradeDao.gradeList(con, pageBean,grade));
+			int total =gradeDao.gradeCount(con,grade);
+			result.put("rows", jsonArray);
+			result.put("total",total);
+			ResponseUtil.write(response, result);
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			try {
+				dbutil.closeCon(con);
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+		}
+	}
+	
+	
+}
